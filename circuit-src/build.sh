36080b07f644d2d6cb75dd3c453ffd38cc46bdc0
Gregory Babski <babskig@gmail.com>
2014-10-14 20:59:18 +0200

    Trying to fix generation

--- circuit-src/build.sh
+++ circuit-src/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-m4 circuit.m4 | dpic -p > circuit.tex
-latex doc.tex
+m4 circuit.m4 | dpic -g > circuit.tex
+pdflatex -shell-escape doc.tex
 convert -density 300 -transparent white doc.ps circuit.png
