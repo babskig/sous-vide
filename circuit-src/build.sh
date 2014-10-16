40eea4c315eacd7fa94d9069b282218723a18684
Gregory Babski <babskig@gmail.com>
2014-10-16 19:53:06 +0200

    Updated build process for supply

--- circuit-src/build.sh
+++ circuit-src/build.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
-rm circuit.png
-rm circuit.tex
-rm doc-figure0.dpth
-rm doc-figure0.log
-rm doc-figure0.pdf
-rm doc.aux
-rm doc.auxlock
-rm doc.log
-rm doc.pdf
-m4 circuit.m4 | dpic -g > circuit.tex
-pdflatex -shell-escape doc.tex
-convert -trim -density 300 -transparent white doc.pdf circuit.png
-cp circuit.png /media/enormo/
+rm ../supply.png
+rm supply.tex
+rm supplydoc-figure0.dpth
+rm supplydoc-figure0.log
+rm supplydoc-figure0.pdf
+rm supplydoc.aux
+rm supplydoc.auxlock
+rm supplydoc.log
+rm supplydoc.pdf
+m4 supply.m4 | dpic -g > supply.tex
+pdflatex -shell-escape supplydoc.tex
+convert -trim -density 300 -transparent white supplydoc.pdf ../supply.png
